af88ea818285403c8151c7576de5cbb6a9d9d2db
Kim Albert <kim.yamamuti@hotmail.com>
2024-05-22 08:49:55 -0300

    first commit

---
+++ json_data.js
@@ -0,0 +1,24 @@
+const json_data = {
+    "name": "Path",
+    "children": [
+     {
+        "name": "DW",
+        "children": [
+         {"name": "Dataset"},
+         {"name": "Dataset"},
+         {"name": "Dataset"},
+         {"name": "Dataset"},
+         {"name": "Dataset"}
+        ]
+     },
+     {
+        "name": "Presto",
+        "children": [
+            {"name": "Dataset"},
+            {"name": "Dataset"}
+        ]
+     }
+    ]
+   };
+
+// console.log(json_data)
